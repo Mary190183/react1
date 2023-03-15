ded6dd962f2a0dcf03639a1f5f2f5cc6926c18bb
Mariya Petrovskaya <pma190183@yandex.ru>
2023-03-15 23:28:21 +0300

    feat: add input search

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,8 +7,21 @@ const Header = () => {
         <Link to="/">
           <h1 className="logo">Houseplant</h1>
         </Link>
+        <form action="" method="get">
+          <input
+            className="input-search"
+            name="search"
+            placeholder="Enter plant ..."
+            type="search"
+          ></input>
+          <button className="button" type="submit">
+            <div className="search-icon"></div>
+          </button>
+        </form>
         <Link to="/about">About</Link>
-        <Link to="/cart">Cart</Link>
+        <Link className="nav-cart" to="/cart">
+          Cart
+        </Link>
       </div>
     </header>
   );
